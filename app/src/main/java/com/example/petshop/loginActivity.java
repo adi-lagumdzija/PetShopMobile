f929e500f2abd8c930e72a56463f613f9fcefa51
adi-lagumdzija <adilag0511@gmail.com>
2021-06-07 13:49:27 +0200

    adding project

---
+++ app/src/main/java/com/example/petshop/loginActivity.java
@@ -0,0 +1,28 @@
+package com.example.petshop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class loginActivity extends AppCompatActivity {
+private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        button = (Button) findViewById(R.id.registerbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+    public void openRegisterActivity() {
+        Intent intent = new Intent(this, registerActivity.class);
+        startActivity(intent);
+    }
+}
